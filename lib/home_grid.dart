cf36f73bead1e8bdd73d51978addfa7c6efc1ea4
Mumett <bayudestar@gmail.com>
2023-09-24 21:05:26 +0700

    buat ulang grid

--- lib/home_grid.dart
+++ lib/home_grid.dart
@@ -1,25 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 //import 'material_indicator_design.dart';
 
-class HomeGridView extends StatelessWidget {
+class HomeGridView extends StatefulWidget {
   const HomeGridView({super.key});
 
   @override
+  State<HomeGridView> createState() => _HomeGridViewState();
+}
+
+class _HomeGridViewState extends State<HomeGridView> {
+  final int _cells = 12;
+  final double _containerSizeSmall = 100;
+  final double _containerSizeLarge = 250;
+  final double _padding = 4;
+  int _clicked = 0;
+
+  final List<String> _imagePaths = [
+    'lib/images/hotel1.jpg',
+    'lib/images/hotel2.jpg',
+    'lib/images/hotel4.jpg',
+    'lib/images/hotel5.jpg',
+    'lib/images/hotel6.jpg',
+    'lib/images/hotel7.jpg',
+    'lib/images/hotel8.jpg',
+    'lib/images/hotel9.jpg',
+    'lib/images/hotel10.jpg',
+    'lib/images/hotel11.jpg',
+    'lib/images/hotel12.jpg'
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: MasonryGridView.builder(
-          itemCount: 12,
-          gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () => Image.asset('lib/images/hotel${index}.jpg', scale: 3),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.asset('lib/images/hotel${index + 1}.jpg')),
+    final Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Container(
+        height: size.height,
+        width: 240,
+        child: Wrap(
+          children: List.generate(
+            _cells,
+            (col) => Padding(
+              padding: EdgeInsets.all(_padding),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _clicked != col + 1 ? _clicked = col + 1 : _clicked = 0;
+                  });
+                },
+                child: Container(
+                  height: _clicked == col + 1
+                      ? _containerSizeLarge
+                      : _containerSizeSmall,
+                  width: _clicked == col + 1
+                      ? _containerSizeLarge
+                      : _containerSizeSmall,
+                  child: _clicked == col + 1
+                      ? Image.asset(
+                          _imagePaths[col %
+                              _imagePaths.length], // Use images cyclically
+                        )
+                      : Center(
+                          child: Image.asset(
+                            _imagePaths[col %
+                                _imagePaths.length], // Use images cyclically
+                          ),
+                        ),
+                ),
+              ),
             ),
           ),
         ),

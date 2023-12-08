be1cfad4dc06b8e57c7f6f04359b40eba26ce428
Mumett <bayudestar@gmail.com>
2023-12-08 12:44:39 +0700

    remove facilities_box and update room_detail

--- lib/view/room_detail.dart
+++ lib/view/room_detail.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hotel_pbp/components/facilities_box.dart';
+// import 'package:hotel_pbp/components/facilities_box.dart';
 
 class RoomDetail extends StatelessWidget {
   const RoomDetail({super.key});
@@ -25,7 +25,7 @@ class RoomDetail extends StatelessWidget {
             const SizedBox(height: 30.0),
             ClipRRect(
               borderRadius:
-                  BorderRadius.circular(15), // Adjust the radius as needed
+                  BorderRadius.circular(15),
               child: Image.asset(
                 'assets/hotel1.jpg', // Replace with your image asset,
                 fit: BoxFit.cover,
@@ -38,6 +38,8 @@ class RoomDetail extends StatelessWidget {
               child: const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+
+                  //Jenis Kamar
                   Center(
                     child: Text(
                       'Standard Room',
@@ -50,22 +52,31 @@ class RoomDetail extends StatelessWidget {
 
                   SizedBox(height: 32.0),
 
+                  //Deskripsi kamar
                   Center(
                     child: Text(
-                      'Room Description', // Replace with your description
-                      style: TextStyle(fontSize: 16.0),
+                      'Room Description',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
 
                   SizedBox(height: 16.0),
+
+                  //Isi deskripsi
                   Center(
                     child: Text(
                       'Kamar Deluxe menawarkan pengalaman menginap yang lebih mewah dengan sentuhan tambahan. kamar lebih luas daripada kamar standar dan dilengkapi dengan fasilitas ekstra, seperti area duduk, perlengkapan kamar mandi berkualitas tinggi, dan pemandangan yang lebih menarik. ',
                       style: TextStyle(fontSize: 12.0),
+                      textAlign: TextAlign.center,
                     ),
                   ),
 
-                  SizedBox(height: 16.0),
+                  SizedBox(height: 22.0),
+
+                  //fasilitas kamar
                   Center(
                     child: Text(
                       'Room Facilities',
@@ -73,71 +84,118 @@ class RoomDetail extends StatelessWidget {
                           fontSize: 16.0, fontWeight: FontWeight.bold),
                     ),
                   ),
-
                   SizedBox(height: 16.0),
-                  
                   Wrap(
                     spacing: 8.0,
                     children: [
-                      // Wrap the FacBox widgets inside a SingleChildScrollView
                       SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Row(
                           children: [
-                            FacBox(text: 'wifi'),
-                            FacBox(text: 'AC'),
-                            FacBox(text: 'TV'),
-                            FacBox(text: 'Breakfast'),
-                            FacBox(text: 'Hair Dryer'),
+                            // FacBox(text: 'wifi'),
+                            // FacBox(text: 'AC'),
+                            // FacBox(text: 'TV'),
+                            // FacBox(text: 'Breakfast'),
+                            // FacBox(text: 'Hair Dryer'),
                             // Add other FacBox widgets with their respective text
                           ],
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(height: 16.0),
 
+                  SizedBox(height: 32.0),
+
+                  //Info hotel
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      // Check-in and check-out time widgets
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.calendar_month),
+                          Text(
+                            'Check-in 11 AM',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.calendar_month),
+                          Text(
+                            'Check-out 12 PM',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                   SizedBox(height: 16.0),
-
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.phone),
+                          Text(
+                            '(123) 555-6789',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.location_city),
+                          Text(
+                            'Jl. Babarsari No. 44',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                   SizedBox(height: 16.0),
-
-                  // SizedBox(height: 16.0),
-                  // Add the contact information and address here
+                  SizedBox(height: 16.0),
                 ],
               ),
             ),
-            Container(
-                color: const Color.fromARGB(255, 175, 61, 49),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Rp.350.000/Night',
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    // const Spacer(), // Expands to fill the remaining space
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.black),
-                      ),
-                      onPressed: () {},
-                      child: const Text('Book Now',
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                  ],
-                ))
+
+            //harga dan button book
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    color: const Color.fromARGB(255, 175, 61, 49),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Rp.350.000 /Night',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        // const Spacer(), // Expands to fill the remaining space
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.black),
+                          ),
+                          onPressed: () {
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //       builder: (context) => TransactionPage()),
+                            // );
+                          },
+                          child: const Text('Book Now',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ],
+                    ))),
           ],
         ),
       ),
